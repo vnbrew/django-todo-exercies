1614dc0f00bbe2032193f16223c0afa28f216bf5
Nguyen Duc Quy <quynd@tech.est-rouge.com>
2023-02-01 11:11:37 +0700

    add feature register new user

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializers
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserRegisterAPIView(APIView):
+    '''
+    Creates the user.
+    '''
+
+    def post(self, request, format='json'):
+        serializer = UserSerializers(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
